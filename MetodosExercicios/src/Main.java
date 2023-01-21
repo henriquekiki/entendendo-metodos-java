6ab6230ec880104ec92e4fba154357ea856960b7
henriquekiki <ricckdmo@gmail.com>
2023-01-21 20:32:32 -0300

    Aperfeiçoamentos

--- MetodosExercicios/src/Main.java
+++ MetodosExercicios/src/Main.java
@@ -1,16 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        /* 
-        System.out.println("Exercicios calculadora");
+         
+        System.out.println("Exercicio calculadora");
         Calculadora.soma(3, 6);
         Calculadora.subtracao(3, 6);
         Calculadora.multiplicacao(3, 6);
         Calculadora.divisao(3, 6);
-        */
-        System.out.println("Exercicios mensagem.");
+        
+        
+        System.out.println("Exercicio mensagem.");
         Mensagem.hora(10);
         Mensagem.hora(14);
         Mensagem.hora(20);
         Mensagem.hora(25);
+        
+
+        System.out.println("Exercicio emprestimo.");
+        Emprestimo.calcular(1000, 1);
+        Emprestimo.calcular(1000, 2);
+        Emprestimo.calcular(1000, 3);
+
+        
     }
 }
