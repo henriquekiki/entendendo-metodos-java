3dc8770111390979e885482e21e6079081218016
henriquekiki <ricckdmo@gmail.com>
2023-01-21 15:13:26 -0300

    Exercicios

--- MetodosExercicios/src/Mensagem.java
+++ MetodosExercicios/src/Mensagem.java
@@ -3,84 +3,41 @@ public class Mensagem {
     public static void hora(int hora){
 
         switch(hora){
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 00:
             case 1:
-                System.out.println("Boa noite!");
-                break;
             case 2:
-                System.out.println("Boa noite!");
-                break;
             case 3:
-                System.out.println("Boa noite!");
-                break;
             case 4:
                 System.out.println("Boa noite!");
                 break;
+            
             case 5:
-                System.out.println("Bom dia!");
-                break;
             case 6:
-                System.out.println("Bom dia!");
-                break;
             case 7:
-                System.out.println("Bom dia!");
-                break;
             case 8:
-                System.out.println("Bom dia!");
-                break;
             case 9:
-                System.out.println("Bom dia!");
-                break;
             case 10:
-                System.out.println("Bom dia!");
-                break;
             case 11:
-                System.out.println("Bom dia!");
-                break;
             case 12:
                 System.out.println("Bom dia!");
                 break;
+            
             case 13:
-                System.out.println("Boa tarde!");
-                break;
             case 14:
-                System.out.println("Boa tarde!");
-                break;
             case 15:
-                System.out.println("Boa tarde!");
-                break;
             case 16:
-                System.out.println("Boa tarde!");
-                break;
             case 17:
-                System.out.println("Boa tarde!");
-                break;
             case 18:
                 System.out.println("Boa tarde!");
                 break;
-            case 19:
-                System.out.println("Boa noite!");
-                break;
-            case 20:
-                System.out.println("Boa noite!");
-                break;
-            case 21:
-                System.out.println("Boa noite!");
-                break;
-            case 22:
-                System.out.println("Boa noite!");
-                break;
-            case 23:
-                System.out.println("Boa noite!");
-                break;
-            case 00:
-                System.out.println("Boa noite!");
-                break;
+            
             default:
                 System.out.println("Hora inválida.\nDigite uma hora entre 00 e 23");
-
-
         }
-        
-        
     }
 }
